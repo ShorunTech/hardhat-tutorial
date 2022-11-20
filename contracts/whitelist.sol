efa4860874e797d17ac549cf3a86e2eeba2c9776
ShorunTech <gshoronke@gmail.com>
2022-11-20 16:29:26 +0100

    whitelist

--- contracts/whitelist.sol
+++ contracts/whitelist.sol
@@ -2,4 +2,11 @@
 pragma solidity ^0.8.0;
 
 
-contract Whitelist 
+contract Whitelist {
+
+    // Max number of whitelisted addresses allowed
+    uint8 public maxWhitelistedAddresses;
+
+    // Create a mapping of whitelistedAddresses
+    // if an address is whitelisted, we would set it to true, it is false by default for all other addresses.
+    mapping(address => bool) public whitelistedAddresses;
