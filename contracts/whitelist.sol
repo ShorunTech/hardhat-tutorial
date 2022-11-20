f68e186683cbcb2d0838b42f81a8a4ff7e01018a
ShorunTech <gshoronke@gmail.com>
2022-11-20 17:42:34 +0100

    whitelist

--- contracts/whitelist.sol
+++ contracts/whitelist.sol
@@ -26,3 +26,16 @@ contract Whitelist {
         addAddressToWhitelist - This function adds the address of the sender to the
         whitelist
      */
+
+      function addAddressToWhitelist() public {
+        // check if the user has already been whitelisted
+        require(!whitelistedAddresses[msg.sender], "Sender has already been whitelisted");
+        // check if the numAddressesWhitelisted < maxWhitelistedAddresses, if not then throw an error.
+        require(numAddressesWhitelisted < maxWhitelistedAddresses, "More addresses cant be added, limit reached");
+        // Add the address which called the function to the whitelistedAddress array
+        whitelistedAddresses[msg.sender] = true;
+        // Increase the number of whitelisted addresses
+        numAddressesWhitelisted += 1;
+    }
+
+}
