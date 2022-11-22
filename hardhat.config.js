3f88f186e02ba7bb72428ccd9e1ce6478e383666
ShorunTech <gshoronke@gmail.com>
2022-11-22 14:17:53 +0100

    hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,9 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+
+  },
+};
