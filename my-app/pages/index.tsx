7ab77bcd2a5f1193017dd26596f368c86bd65a0f
Shoronke Gbenga <gshoronke@gmail.com>
2022-11-26 15:14:36 +0100

    index

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -4,3 +4,13 @@ import Web3Modal from "web3modal";
 import { providers, Contract } from "ethers";
 import { useEffect, useRef, useState } from "react";
 import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
+
+
+
+export default function Home() {
+    // walletConnected keep track of whether the user's wallet is connected or not
+    const [walletConnected, setWalletConnected] = useState(false);
+    // joinedWhitelist keeps track of whether the current metamask address has joined the Whitelist or not
+    const [joinedWhitelist, setJoinedWhitelist] = useState(false);
+    // loading is set to true when we are waiting for a transaction to get mined
+    const [loading, setLoading] = useState(false);
