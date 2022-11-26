5680492e0a10f7981e4e7fe51d64f76903e88ab8
Shoronke Gbenga <gshoronke@gmail.com>
2022-11-26 15:13:42 +0100

    index

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -2,3 +2,5 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
 import { providers, Contract } from "ethers";
+import { useEffect, useRef, useState } from "react";
+import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
