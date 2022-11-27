09c55f4edd5285ef5813333563a4406310412fb1
Shoronke Gbenga <gshoronke@gmail.com>
2022-11-27 16:57:56 +0100

    index

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -37,3 +37,10 @@ export default function Home() {
     // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
     const provider = await web3ModalRef.current.connect();
     const web3Provider = new providers.Web3Provider(provider);
+
+    // If user is not connected to the Goerli network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 5) {
+      window.alert("Change the network to Goerli");
+      throw new Error("Change network to Goerli");
+    }
